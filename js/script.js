cdbb0d34cc50ca11a0b4a6c46259005bbea7531a
Guillaume Perbet <perbet.dev@gmail.com>
2020-07-29 11:10:18 +0200

    delete modal trigger

--- js/script.js
+++ js/script.js
@@ -2,9 +2,26 @@
 
 //details modal trigger
 $('#detailsModal').on('show.bs.modal', function (event) {
+    //get product id
     const button = $(event.relatedTarget);
     const productId = button.data('id');
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // fetch product details
+
+    //add data-id to edit button
     const editBtn = document.getElementById('edit-btn');
     editBtn.setAttribute('data-id',productId);
+})
+
+//delete modal trigger
+$('#deleteModal').on('show.bs.modal', function (event) {
+    //get product name and id
+    const button = $(event.relatedTarget);
+    const productId = button.data('id');
+    const productName = button.data('name');
+    //add product name
+    const name = document.getElementById('delete-name');
+    name.innerText = productName;
+    //add data-id to delete button
+    const deleteBtn = document.getElementById('delete-btn');
+    deleteBtn.setAttribute('data-id',productId);
 })
