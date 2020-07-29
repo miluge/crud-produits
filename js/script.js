166ce25e97c0f54f04753c5ee643eb7145016f41
Guillaume Perbet <perbet.dev@gmail.com>
2020-07-29 12:02:13 +0200

    add create form modal trigger

--- js/script.js
+++ js/script.js
@@ -5,11 +5,34 @@ $('#detailsModal').on('show.bs.modal', function (event) {
     //get product id
     const button = $(event.relatedTarget);
     const productId = button.data('id');
-    // fetch product details
+
+    //fetch product details
+    //render details template
+    //insert in details modal
 
     //add data-id to edit button
     const editBtn = document.getElementById('edit-btn');
     editBtn.setAttribute('data-id',productId);
+})
+
+//edit/create form modal trigger
+$('#formModal').on('show.bs.modal', function (event) {
+    //check product id
+    const button = $(event.relatedTarget);
+    const productId = button.data('id')
+    if (productId){//edit mode
+
+        //fetch edit-product.php
+        //render form template in 'Edit' mode with product details
+        //insert response in form modal
+
+    }else{
+
+        //fetch add-product.php
+        //render form template in 'Add' mode
+        //insert response in form modal
+
+    }
 })
 
 //delete modal trigger
