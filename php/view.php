1640821db204bca7d61e567111baa471d3c2f0e5
Guillaume Perbet <perbet.dev@gmail.com>
2020-07-30 14:47:45 +0200

    add categories in form

--- php/view.php
+++ php/view.php
@@ -6,11 +6,17 @@ $pdo = pdo_connect_mysql();
 
 if (isset($_POST['mode'])){
     $mode = $_POST['mode'];
-    $twig = load_twig();
+    
+    //load twig
+    require_once '../vendor/autoload.php';
+    $loader = new \Twig\Loader\FilesystemLoader('../templates/');
+    $twig = new \Twig\Environment($loader, [
+        'cache' => false,
+    ]);
 
     if (isset($_POST['id'])){//get product by id
         $id = $_POST['id'];
-        $stmt = $pdo->prepare("SELECT id_products, img.name AS image , cat.name AS category , man.name AS manual, src.name AS source, products.name, reference_number, price, buy_date, end_warranty, care_products FROM products INNER JOIN image AS img ON products.image_id = img.id_image INNER JOIN category AS cat ON products.category_id = cat.id_category INNER JOIN manual AS man ON products.manual_id = man.id_manual INNER JOIN source AS src ON products.source_id = src.id_source WHERE id_products = :id");
+        $stmt = $pdo->prepare("SELECT id_products, img.name AS image , cat.name AS category, cat.id_category AS id_category , man.name AS manual, src.name AS source, products.name, reference_number, price, buy_date, end_warranty, care_products FROM products INNER JOIN image AS img ON products.image_id = img.id_image INNER JOIN category AS cat ON products.category_id = cat.id_category INNER JOIN manual AS man ON products.manual_id = man.id_manual INNER JOIN source AS src ON products.source_id = src.id_source WHERE id_products = :id");
         $stmt->execute([':id'=>$id]);
         $product = $stmt->setFetchMode(PDO::FETCH_ASSOC);
         $product = $stmt->fetch();
@@ -26,16 +32,14 @@ if (isset($_POST['mode'])){
         echo $twig->render( 'details.html.twig' , $product);
     }
 
-    if ($mode == 'Edit'){//return edit form render
+    if ($mode == 'Edit' || $mode == 'Add'){//return form render
+        //get categories
+        $stmt = $pdo->prepare("SELECT id_category, name FROM category");
+        $stmt->execute();
+        $product['categories'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
         $product['mode'] = $mode;
         echo $twig->render( 'form.html.twig' , $product);
     }
-
-    if ($mode == 'Add'){//return add form render
-        $product['mode'] = $mode;
-        echo $twig->render( 'form.html.twig' , $product);
-    }
-    
 }else{//if no $_POST['mode'], return products table entries for index.php
 
     // Prepare the SQL statement and get records from products table
