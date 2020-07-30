9a1d52862759376c767d663532dbb7e380ffba3e
Guillaume Perbet <perbet.dev@gmail.com>
2020-07-30 11:16:23 +0200

    use complete request

--- php/view.php
+++ php/view.php
@@ -10,7 +10,7 @@ if (isset($_POST['mode'])){
 
     if (isset($_POST['id'])){//get product by id
         $id = $_POST['id'];
-        $stmt = $pdo->prepare("SELECT * FROM products WHERE id_products = :id");
+        $stmt = $pdo->prepare("SELECT id_products, img.name AS image , cat.name AS category , man.name AS manual, src.name AS source, products.name, reference_number, price, buy_date, end_warranty, care_products FROM products INNER JOIN image AS img ON products.image_id = img.id_image INNER JOIN category AS cat ON products.category_id = cat.id_category INNER JOIN manual AS man ON products.manual_id = man.id_manual INNER JOIN source AS src ON products.source_id = src.id_source WHERE id_products = :id");
         $stmt->execute([':id'=>$id]);
         $product = $stmt->setFetchMode(PDO::FETCH_ASSOC);
         $product = $stmt->fetch();
@@ -39,13 +39,11 @@ if (isset($_POST['mode'])){
 }else{//if no $_POST['mode'], return products table entries for index.php
 
     // Prepare the SQL statement and get records from products table
-    // $stmt = $pdo->prepare('SELECT id_products, img.name, cat.name AS category, man.name, src.name, products.name, reference_number, price, buy_date, end_warranty, care_products FROM products INNER JOIN image AS img ON products.image_id = img.id_image INNER JOIN category AS cat ON products.category_id = cat.id_category INNER JOIN manual AS man ON products.manual_id = man.id_manual INNER JOIN source AS src ON products.source_id = src.id_source');
-    $stmt = $pdo->prepare('SELECT * FROM products');
+    $stmt = $pdo->prepare('SELECT id_products, img.name AS image , cat.name AS category , man.name AS manual, src.name AS source, products.name, reference_number, price, buy_date, end_warranty, care_products FROM products INNER JOIN image AS img ON products.image_id = img.id_image INNER JOIN category AS cat ON products.category_id = cat.id_category INNER JOIN manual AS man ON products.manual_id = man.id_manual INNER JOIN source AS src ON products.source_id = src.id_source');
     $stmt->execute();
     // Fetch the records so we can display them in template
     $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
     foreach($products as $product){
-        // var_dump($product);
         //render entry template
         echo $twig->render( 'entry.html.twig' , $product);
     }
