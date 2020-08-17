7dfc30f64608c37195d836dd42836f22aefb0201
miluge <gllmblndl@gmail.com>
2020-08-17 13:35:01 +0200

    removed manual uploads

--- php/insert-product.php
+++ php/insert-product.php
@@ -2,6 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors',1);
 
+var_dump($_FILES);
+
 include 'functions.php';
 $pdo = pdo_connect_mysql();
 $msg = '';
@@ -26,85 +28,15 @@ $care_products = isset($_POST['care_products']) ? $_POST['care_products'] : '';
 $stmt->execute();
 $msg = 'Created Successfully!'; 
 
-// Close connection
-unset($pdo);
-
-//     if(isset($_POST['submit'])) {
-//         // for input => ticket image
-        
-//         $ticket = $_FILES['ticket'];
-
-//         $ticketName = $_FILES['ticket']['name'];
-//         $ticketTmpname = $_FILES['ticket']['tmp_name'];
-//         $ticketSize = $_FILES['ticket']['size'];
-//         $ticketError = $_FILES['ticket']['error'];
-//         $ticketType = $_FILES['ticket']['type'];
-
-//         $ticketExt = explode('.', $ticketName);
-//         $ticketActualExt = strtolower(end($fileExt));
-
-//         $allowed = array('jpg', 'jpeg', 'png');
-
-//         if (in_array($ticketActualExt, $allowed)){
-//             if ($ticketError === 0) {
-//                 if ($ticketSize < 500000 ) {
-//                     $ticketNameNew = uniqid(', true').".".$ticketActualExt;
-//                     $ticketDestination = 'uploads/images/'.$ticketNameNew;
-//                     move_uploaded_file($ticketTmpname, $ticketDestination);
-//                 } else {
-//                     echo "file is too big";
-//                 }
-//             } else {
-//             echo "Error uploading your file!";
-//         }
-//     } else {
-//         echo "You cannot upload files of this type!";
-//     }
-
-//     $manual = $_FILES['manual'];
-
-//     $manualName = $_FILES['manual']['name'];
-//     $manualTmpname = $_FILES['manual']['tmp_name'];
-//     $manualSize = $_FILES['manual']['size'];
-//     $manualError = $_FILES['manual']['error'];
-//     $manualType = $_FILES['manual']['type'];
-
-//     $manualExt = explode('.', $manualName);
-//     $manualActualExt = strtolower(end($fileExt));
-
-//     $allowed = array('pdf', 'doc', 'txt');
-
-//     if (in_array($manualActualExt, $allowed)){
-//         if ($manualError === 0) {
-//             if ($manualSize < 500000 ) {
-//                 $manualNameNew = uniqid(', true').".".$manualActualExt;
-//                 $manualDestination = 'uploads/manuals/'.$manualNameNew;
-//                 move_uploaded_file($manualTmpname, $manualDestination);
-//             } else {
-//                 echo "file is too big";
-//             }
-//         } else {
-//         echo "Error uploading your file!";
-//     }
-// } else {
-//     echo "You cannot upload files of this type!";
-// }
-
-// SECOND IMAGE AND MANUAL Method"
-
 if(isset($_POST['submit'])){
 
     // Prepared statement
-    $sql = "INSERT INTO image (name,image) VALUES(?,?)";
+    $sql = "INSERT INTO image (name) VALUES(?)";
     
   
     $statement = $conn->prepare($sql);
-  
-    // Loop all files
-    for($i=0;$i<$countfiles;$i++){
-  
       // File name
-      $filename = $_FILES['files']['name'][$i];
+      $filename = $_FILES['image_url']['name'][$i];
   
       // Location
       $target_file = 'uploads/images'.$filename;
@@ -124,47 +56,42 @@ if(isset($_POST['submit'])){
             // Execute query
         $statement->execute(array($filename,$target_file));
   
-         }
       }
     }
     echo "File upload successfully";
   }
 
-  if(isset($_POST['add-manual'])){
+//   if(isset($_POST['submit'])){
 
-    // Prepared statement
-    $sql = "INSERT INTO manual (name) VALUES(?)";
+//     // Prepared statement
+//     $sql = "INSERT INTO manual (name) VALUES(?)";
     
   
-    $statement = $conn->prepare($sql);
+//     $statement = $conn->prepare($sql)
   
-    // Loop all files
-    for($i=0;$i<$countfiles;$i++){
+//       // File name
+//       $filename = $_FILES['manual_url']['name'][$i];
   
-      // File name
-      $filename = $_FILES['files']['name'][$i];
+//       // Location
+//       $target_file = 'uploads/manuals'.$filename;
   
-      // Location
-      $target_file = 'uploads/manuals'.$filename;
+//       // file extension
+//       $file_extension = pathinfo($target_file, PATHINFO_EXTENSION);
+//       $file_extension = strtolower($file_extension);
   
-      // file extension
-      $file_extension = pathinfo($target_file, PATHINFO_EXTENSION);
-      $file_extension = strtolower($file_extension);
+//       // Valid image extension
+//       $valid_extension_img = array("pdf","txt");
   
-      // Valid image extension
-      $valid_extension_img = array("pdf","txt");
+//       if(in_array($file_extension, $valid_extension)){
   
-      if(in_array($file_extension, $valid_extension)){
+//          // Upload file
+//          if(move_uploaded_file($_FILES['files']['tmp_name'][$i],$target_file)){
   
-         // Upload file
-         if(move_uploaded_file($_FILES['files']['tmp_name'][$i],$target_file)){
+//             // Execute query
+//         $statement->execute(array($filename,$target_file));
   
-            // Execute query
-        $statement->execute(array($filename,$target_file));
-  
-         }
-      }
-    }
-    echo "File upload successfully";
-  }
-}
+//       }
+//     }
+//     echo "File upload successfully";
+//   }
+// }
