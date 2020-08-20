eee3cc3e13d0e441320cac58fb26cebf5cf085dd
Guillaume Perbet <perbet.dev@gmail.com>
2020-08-20 12:01:48 +0200

    delete files

--- php/delete-product.php
+++ php/delete-product.php
@@ -2,10 +2,23 @@
 session_start();
 include 'functions.php';
 if(isset($_POST['id']) && check_user()){
-    //delete from database
+    $id = $_POST['id'];
+    //delete files
     $pdo = pdo_connect_mysql();
-    $stmt = $pdo->prepare('DELETE FROM products WHERE id_products = ?');
-    $stmt->execute([$_POST['id']]);
+    $stmt = $pdo->prepare('SELECT image_url, manual_url FROM products WHERE id_products = :id');
+    $stmt->bindValue(':id', $id);
+    $stmt->execute();
+    $files = $stmt->fetch(PDO::FETCH_ASSOC);
+    $image = "../uploads/images/".$id."-".$files["image_url"];
+    unlink($image);
+    if ($files["manual_url"] != ""){
+        $manual = "../uploads/manuals/".$id."-".$files["manual_url"];
+        unlink($manual);
+    }
+    //delete from database
+    $stmt = $pdo->prepare('DELETE FROM products WHERE id_products = :id');
+    $stmt->bindValue(':id', $id);
+    $stmt->execute();
 }else{
     header('Location:../login.php');
 }
